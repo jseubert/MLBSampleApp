186745e2b9060951194c0721b395d000bab347b5
John Seubert <john.seubert@airbnb.com>
2018-03-21 17:35:25 -0700

    Initial Commit

---
+++ MLBSampleApp/MLBViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MLBViewController.swift
+//  MLBSampleApp
+//
+//  Created by jseubert on 3/21/18.
+//  Copyright © 2018 jseubert. All rights reserved.
+//
+
+import UIKit
+
+class MLBViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Today's Games"
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
